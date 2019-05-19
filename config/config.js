263bdc45d84f3c5d2f4d2b7a791f0fca257d7f48
anhaemin <kebinkastora@gmail.com>
2019-05-19 16:16:56 +0900

    package.json 생성, NodeBird의 conifg.models.passport 복붙

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorAliases: 'false'
+  },
+  production: {
+    username: 'root',
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorAliases: 'false',
+    logging: false
+  }
+}
+
