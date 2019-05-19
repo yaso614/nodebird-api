499b7f4f0879a6df2b7ead0e1391f7f06018a137
anhaemin <kebinkastora@gmail.com>
2019-05-19 23:12:40 +0900

    typying error

--- routes/v1.js
+++ routes/v1.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken } = requier('./middlewares');
+const { verifyToken } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
